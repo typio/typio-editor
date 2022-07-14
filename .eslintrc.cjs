663970ada451649adad91e4c5c372850cb24fdd0
Thomas Huber <me@tohuber.com>
2022-07-14 00:54:15 -0700

    Added prettier and added details to eslint :eyes:

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,15 +1,26 @@
 module.exports = {
-	root: true,
-	extends: ['eslint:recommended'],
-	plugins: ['svelte3'],
-	overrides: [{ files: ['*.svelte'], processor: 'svelte3/svelte3' }],
-	parserOptions: {
-		sourceType: 'module',
-		ecmaVersion: 2020
-	},
-	env: {
-		browser: true,
-		es2017: true,
-		node: true
-	}
-};
+    root: true,
+    extends: 'eslint:recommended',
+    plugins: ['svelte3'],
+    overrides: [
+        {
+            files: ['*.svelte'],
+            processor: 'svelte3/svelte3'
+        }
+    ],
+    parserOptions: {
+        sourceType: 'module',
+        ecmaVersion: 'latest'
+    },
+    env: {
+        browser: true,
+        es2021: true,
+        node: true
+    },
+    rules: {
+        indent: ['error', 4],
+        'linebreak-style': ['error', 'unix'],
+        quotes: ['error', 'single'],
+        semi: ['error', 'never']
+    }
+}
